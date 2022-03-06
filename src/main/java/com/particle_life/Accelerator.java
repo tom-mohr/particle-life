1c06f5a13e576b0b4958b031aac281c82ae8a740
tom-mohr <24864629+tom-mohr@users.noreply.github.com>
2022-03-07 00:54:38 +0100

    vector returned by accelerator is scaled by rmax

--- src/main/java/com/particle_life/Accelerator.java
+++ src/main/java/com/particle_life/Accelerator.java
@@ -5,11 +5,13 @@ import org.joml.Vector3d;
 public interface Accelerator {
 
     /**
-     * Implementations of this interface are allowed to modify <code>x</code>.
-     * So, instead of allocating a new vector, they can modify and return <code>x</code>.
+     * Implementations of this interface are allowed to modify <code>pos</code>.
+     * So, instead of allocating a new vector, they can modify and return <code>pos</code>.
      * @param a
-     * @param pos position of the neighbor relative to the particle's own position, with its length normalized to rmax
-     * @return
+     * @param pos position of the neighbor relative to the particle's own position,
+     *            with its length divided by rmax. (So this vector will always have a length <= 1.)
+     * @return the acceleration that should be applied to the particle's velocity.
+     *         This is also interpreted as relative to rmax, that is, it will be scaled by rmax before it is applied to the particle.
      */
     Vector3d accelerate(double a, Vector3d pos);
 }
